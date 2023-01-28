d9d12d7407c6adf4e2bf24ad37af095e9f80068e
victorpolisetty <52013101+victorpolisetty@users.noreply.github.com>
2023-01-28 18:59:24 -0500

    First Page Almost Done

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,12 @@ import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   // This widget is the root of your application.
   @override
@@ -31,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Vitaminutes'),
+      home: const MyHomePage(title: 'VitaMinutes'),
     );
   }
 }
@@ -48,6 +46,11 @@ class _MyHomePageState extends State<MyHomePage> {
   double myLat = 0;
   double myLong = 0;
   double myAlt = 0;
+  String weightValue = "99";
+  String ageValue = "0";
+  String genderValue = "NA";
+  String skinColorValue = "NA";
+
   @override
   void initState() {
     // TODO: implement initState
@@ -70,49 +73,163 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF4BB96),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'WELCOME TO',
-              style: TextStyle(fontSize: 20),
-            ),
-            RichText(
-              text: new TextSpan(
-                style: new TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.black,
+      body:Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                    'WELCOME TO',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                RichText(
+                  text: new TextSpan(
+                    style: new TextStyle(
+                      fontSize: 40.0,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      new TextSpan(
+                          text: 'Vita',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF995201))),
+                      new TextSpan(
+                          text: 'Minutes',
+                          style: new TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.white)),
+                    ],
+                  ),
                 ),
-                children: <TextSpan>[
-                  new TextSpan(
-                      text: 'Vita',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF995201))),
-                  new TextSpan(
-                      text: 'minutes',
-                      style: new TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.white)),
-                ],
-              ),
-            ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(110,50,0,0),
+                  child: Row(
+                    children: [
+                      Text("Age: ", style: TextStyle(fontSize: 20),),
+                      DropdownButton(
+                          menuMaxHeight: 200,
+                          value: ageValue,
+                          items:
+                          List<String>.generate(101, (int index) => '${index}')
+                              .map(
+                                (val) {
+                              return DropdownMenuItem<String>(
+                                value: val,
+                                child: val == '0' ? Text('Select Age') : Text(val),
+                              );
+                            },
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              ageValue = value.toString();
+                            });
+                          }),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(95,20,0,0),
+                  child: Row(
+                    children: [
+                      Text("Weight: ", style: TextStyle(fontSize: 20),),
+                      DropdownButton(
+                          menuMaxHeight: 200,
+                          value: weightValue,
+                          items:
+                          List<String>.generate(250, (int index) => '${index + 99}')
+                              .map(
+                                (val) {
+                              return DropdownMenuItem<String>(
+                                value: val,
+                                child: val == '99' ? Text('Select Weight') : Text(val),
+                              );
+                            },
+                          ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              weightValue = value.toString();
+                            });
+                          }),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(100,20,0,0),
+                  child: Row(
+                    children: [
+                      Text("Gender: ", style: TextStyle(fontSize: 20),),
+                      DropdownButton(
+                          menuMaxHeight: 200,
+                          value: genderValue,
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Select Gender"),
+                              value: "NA",
+                            ),
+                            DropdownMenuItem(
+                              child: Text("Male"),
+                              value: "Male",
+                            ),
+                            DropdownMenuItem(
+                                child: Text("Female"), value: "Female"),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              genderValue = value.toString();
+                            });
+                          }),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(80,20,0,0),
+                  child: Row(
+                    children: [
+                      Text("Skin Color: ", style: TextStyle(fontSize: 20),),
+                      DropdownButton(
+                          menuMaxHeight: 200,
+                          value: skinColorValue,
+                          items: [
+                            DropdownMenuItem(
+                              child: Text("Select Skin Color"),
+                              value: "NA",
+                            ),
+                            DropdownMenuItem(
+                              child: Icon(Icons.circle, color: Color(0xFFFFE2CC),),
+                              value: "1",
+                            ),
+                            // DropdownMenuItem(
+                            //     child: Text("Female"), value: "2"),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              skinColorValue = value.toString();
+                            });
+                          }),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50,),
+                TextButton(
+                  onPressed: () {
+                    callAPI();
+                  },
+                  child: Text(
+                    'CONTINUE',
+                    style: TextStyle(color: Colors.black, fontSize: 15),
+                  ),
+                ),
 
-            TextButton(
-              onPressed: () { callAPI();},
-              child: Text('CONTINUE', style: TextStyle(color: Colors.black, fontSize: 15),),
+              ],
             ),
-            SizedBox(
-              height: 450,
-            ),
-
-          ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
   void callAPI() async {
-    var url = Uri.parse('https://api.openuv.io/api/v1/forecast?lat=$myLat&lng=$myLong&alt=$myAlt&dt=');
+    Map<String, dynamic> data;
+    var url = Uri.parse(
+        'https://api.openuv.io/api/v1/uv?lat=$myLat&lng=$myLong&alt=$myAlt');
     var apiKey = 'openuv-2lxbtldfix8q8-io';
 
     var headers = {
@@ -123,11 +240,13 @@ class _MyHomePageState extends State<MyHomePage> {
     var response = await http.get(url, headers: headers);
 
     if (response.statusCode == 200) {
-      print(response.body);
+      data = jsonDecode(response.body);
+      print(data);
     } else {
       print('Request failed with status: ${response.statusCode}');
     }
   }
+
   /// Determine the current position of the device.
   ///
   /// When the location services are not enabled or permissions
@@ -172,6 +291,5 @@ class _MyHomePageState extends State<MyHomePage> {
     myLong = position.longitude;
     myAlt = position.altitude;
     return await Geolocator.getCurrentPosition();
-
   }
 }
